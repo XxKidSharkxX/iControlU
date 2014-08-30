f93c3325ab0e911b1ed2208fc997fa9829d4b563
Firebreath15 <hadengw@gmail.com>
2014-08-30 17:25:57 -0600

    Updated to work with Craftbukkit Build #03116

--- iControlU/src/me/firebreath15/icontrolu/iListener.java
+++ iControlU/src/me/firebreath15/icontrolu/iListener.java
@@ -1,11 +1,11 @@
 package me.firebreath15.icontrolu;
 
-import net.minecraft.server.v1_7_R3.EntityPlayer;
-import net.minecraft.server.v1_7_R3.PacketPlayInArmAnimation;
+import net.minecraft.server.v1_7_R4.EntityPlayer;
+import net.minecraft.server.v1_7_R4.PacketPlayInArmAnimation;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.craftbukkit.v1_7_R3.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
