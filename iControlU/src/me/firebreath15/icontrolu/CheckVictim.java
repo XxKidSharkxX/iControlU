092561bbe7d4d8a0d2880d1e3cd82e3cbbc4e51d
Firebreath15 <hadengw@gmail.com>
2015-05-19 21:29:51 -0600

    v1.9.1: Fixed some issues

--- iControlU/src/me/firebreath15/icontrolu/CheckVictim.java
+++ iControlU/src/me/firebreath15/icontrolu/CheckVictim.java
@@ -15,15 +15,23 @@ public class CheckVictim extends BukkitRunnable{
 	
 	public void run(){
 		if(v != null && c != null){
-			if(v.getWorld().getName().equalsIgnoreCase(c.getWorld().getName())){
-				if(v.getLocation().distance(c.getLocation()) > 25){
-					v.teleport(c);
-					TitleAPI.sendTitle(v, 20, 20, 60, "§4TOO FAR AWAY", "");
+			if(v.hasMetadata("iCU_P")){
+				if(c.hasMetadata("iCU_H")){
+					if(v.getWorld().getName().equalsIgnoreCase(c.getWorld().getName())){
+						if(v.getLocation().distance(c.getLocation()) > 25){
+							v.teleport(c);
+							TitleAPI.sendTitle(v, 20, 20, 60, "§4TOO FAR AWAY", "");
+						}
+					}else{
+						v.teleport(c);
+						v.setGameMode(GameMode.SPECTATOR);
+						TitleAPI.sendTitle(v, 20, 20, 60, "§4SWITCHED WORLDS", "");
+					}
+				}else{
+					this.cancel();
 				}
 			}else{
-				v.teleport(c);
-				v.setGameMode(GameMode.SPECTATOR);
-				TitleAPI.sendTitle(v, 20, 20, 60, "§4SWITCHED WORLDS", "");
+				this.cancel();
 			}
 		}else{
 			this.cancel();
